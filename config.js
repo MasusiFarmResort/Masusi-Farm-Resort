12ed57fb58ffe6ec688a1ef431e1e7e790c284c6
MasusiFarmResort <farmmasusi@gmail.com>
2026-09-11 15:40:36 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.MASUSI_CONFIG = {
-  SUPABASE_URL: "PASTE_YOUR_SUPABASE_URL_HERE",
-  SUPABASE_ANON_KEY: "PASTE_YOUR_SUPABASE_ANON_KEY_HERE",
+  SUPABASE_URL: "https://lmzafxveejeskoqatwjj.supabase.co/",
+  SUPABASE_ANON_KEY: "sb_publishable_jIBkgpAttO9QFBbvpBrz1A_SNF-kUmR",
   STORAGE_BUCKET: "resort-assets",
   DEFAULT_RESORT_NAME: "Masusi Farm Resort"
 };
